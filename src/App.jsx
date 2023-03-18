2f312ceea3c1c797ad47513511f9ab088d460bb2
hori19 <fourbasic19@gmail.com>
2023-03-18 19:26:53 +0900

    【jsx】Formコンポーネント作成　js操作は無し

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Title from './components/Title'
+import Form from './components/Form'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
         <Title />
+        <Form />
     </div>
   )
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+const Form = () => {
+    return(
+        <form>
+            <input type="text" name="keyword" placeholder="e.g. cat" />
+            <button type="submit">Search</button>
+        </form>
+    );
+}
+ 
+export default Form;
