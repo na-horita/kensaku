ec01cab6bb43ced635f419577bde2e59348e79d0
hori19 <fourbasic19@gmail.com>
2023-03-19 19:30:25 +0900

    【jsx】pexelsAPIを使用して、２つ目のサイトから画像をゲットして、マージンして、一覧表示。

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,63 @@ import './App.css'
 
 function App() {
   const [word, setWord] = useState('')
-  const [photo,setPhoto] = useState([])
+  const [photos,setPhotos] = useState([])
+
+  const searchImages = async () => {
+    const pexelsAPIKey = 'ycuX09ywi56e2xu3hSuMWNDw4vzJAyye7HKi7LYQIoEUz0QnoQC0sE7S';
+    const unsplashAPIKey = 'UrTEHHwYhYpE612HSg7bfj7KPAHUADyp1YCJsoLEcL8';
+
+    // Pexels APIのリクエスト
+    const pexelsResponse = await axios.get(`https://api.pexels.com/v1/search?query=${word}&per_page=10`, {
+      headers: {
+        Authorization: pexelsAPIKey,
+      },
+    });
+    const pexelsPhotos = pexelsResponse.data.photos.map((photo) => ({
+      id: photo.id,
+      source: 'pexels',
+      url: photo.src.medium,
+      link: photo.url,
+      photographer: photo.photographer,
+      created_at: photo.created_at,
+    }));
+
+    // Unsplash APIのリクエスト
+    const unsplashResponse = await axios.get(`https://api.unsplash.com/search/photos?query=${word}&per_page=10`, {
+      headers: {
+        Authorization: `Client-ID ${unsplashAPIKey}`,
+      },
+    });
+    const unsplashPhotos = unsplashResponse.data.results.map((photo) => ({
+      id: photo.id,
+      source: 'unsplash',
+      url: photo.urls.regular,
+      link: photo.links.html,
+      photographer: photo.user.name,
+      created_at: photo.created_at,
+    }));
+
+    // PexelsとUnsplashの結果を合わせる
+    const mergedPhotos = [...pexelsPhotos, ...unsplashPhotos];
+
+    // 作成日の新しい順にソートする
+    mergedPhotos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+    // 結果をセットする
+    setPhotos(mergedPhotos);
+  };
 
   const getPhotoData = (e) => {
     e.preventDefault();
-    axios
-    .get('https://api.unsplash.com/search/photos?query=cat&client_id=UrTEHHwYhYpE612HSg7bfj7KPAHUADyp1YCJsoLEcL8')
-    .then(res => {
-      setPhoto(res.data.results)
-      })
-  }
+    searchImages();
+  };
 
   return (
     <div className="App">
         <Title />
         <Form setWord={setWord} getPhotoData={getPhotoData} />
         検索文字:{word}
-        <Results photo = {photo} />
+        <Results photos = {photos} />
     </div>
   )
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,10 @@
-const Results = ({photo}) => {
+const Results = ({photos}) => {
     return (
         <div className="photo-list">
-            {photo.map((singleData) =>
-                <a href={singleData.links.html}>
-                    <img src={singleData.urls.regular} alt={singleData.alt_description} />
+            {photos.map((singleData) =>
+                <a href={singleData.link} target="_blank">
+                    <img src={singleData.url} alt={singleData.photographer} />
+                    <p>{singleData.source}:{singleData.created_at}</p>
                 </a>
             )}
         </div>
