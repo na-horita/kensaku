2db1d6f17a841ef26782d25d2767de45f28dd1f2
hori19 <fourbasic19@gmail.com>
2023-03-23 15:45:29 +0900

    【jsx】App.jsページに記述していた画像検索アプリのコードをTop.jsxに移動

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,37 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
-import Form from './components/Form'
-import Results from './components/Results'
-import Frequent from './components/Frequent';
 import './App.css'
 import Top from './pages/Top';
 import Contact from './pages/Contact';
 
 function App() {
-  const [word, setWord] = useState('')
-  const [photos,setPhotos] = useState([])
-  const [loading,setLoading] = useState(false)
-
-  //初期値をuseStateで保持している変数のwordにした。そしてこの変数は外部から代入することが可能としている。
-  const searchImages = async (word2 = word) => {
-    const pexelsAPIKey = 'ycuX09ywi56e2xu3hSuMWNDw4vzJAyye7HKi7LYQIoEUz0QnoQC0sE7S';
-    const unsplashAPIKey = 'UrTEHHwYhYpE612HSg7bfj7KPAHUADyp1YCJsoLEcL8';
-
-    // Pexels APIのリクエスト 最大８０件まで
-    const pexelsResponse = await axios.get(`https://api.pexels.com/v1/search?query=${word2}&per_page=10`, {
-      headers: {
-        Authorization: pexelsAPIKey,
-      },
-    });
-    const pexelsPhotos = pexelsResponse.data.photos.map((photo) => ({
-      id: photo.id,
-      source: 'pexels',
-      url: photo.src.medium,
-      link: photo.url,
-      photographer: photo.photographer,
-      created_at: photo.created_at,
-    }));
-
-    // Unsplash APIのリクエスト 最大３０件まで
-    const unsplashResponse = await axios.get(`https://api.unsplash.com/search/photos?query=${word2}&per_page=10`, {
-      headers: {
-        Authorization: `Client-ID ${unsplashAPIKey}`,
-      },
-    });
-    const unsplashPhotos = unsplashResponse.data.results.map((photo) => ({
-      id: photo.id,
-      source: 'unsplash',
-      url: photo.urls.regular,
-      link: photo.links.html,
-      photographer: photo.user.name,
-      created_at: photo.created_at,
-    }));
-
-    // PexelsとUnsplashの結果を合わせる
-    const mergedPhotos = [...pexelsPhotos, ...unsplashPhotos];
-
-    // 作成日の新しい順にソートする
-    mergedPhotos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-
-    // 結果をセットする
-    setPhotos(mergedPhotos);
-  };
-
-  const getPhotoData = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    await searchImages();
-    setLoading(false);
-  };
 
   return (
     <div className="App">
 
         <BrowserRouter>
-          {/* 2. Routesコンポーネントを使用して、各ページのルートを設定 */}
+          <header>
+            {/* メニュー */}
+            <nav>
+              <ul>
+                <li>
+                  {/* Topページへのリンク */}
+                  <Link to="/">Top</Link>
+                </li>
+                <li>
+                  {/* Contactページへのリンク */}
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          {/* ルーティング */}
           <Routes>
             <Route path="/" element={<Top />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </BrowserRouter>
-
-        <Frequent setWord={setWord} searchImages={searchImages} loading = {loading} setLoading={setLoading} />
-        <Form setWord={setWord} word={word} getPhotoData={getPhotoData} />
-        検索文字:{word}
-        <Results photos = {photos} loading = {loading} />
     </div>
   )
 }
--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,9 +1,81 @@
+import { useState } from 'react'
+
+import axios from 'axios'
+
+import Form from '../components/Form'
+import Results from '../components/Results'
+import Frequent from '../components/Frequent';
+
 const Top = () => {
+
+  const [word, setWord] = useState('')
+  const [photos,setPhotos] = useState([])
+  const [loading,setLoading] = useState(false)
+
+  //初期値をuseStateで保持している変数のwordにした。そしてこの変数は外部から代入することが可能としている。
+  const searchImages = async (word2 = word) => {
+    const pexelsAPIKey = 'ycuX09ywi56e2xu3hSuMWNDw4vzJAyye7HKi7LYQIoEUz0QnoQC0sE7S';
+    const unsplashAPIKey = 'UrTEHHwYhYpE612HSg7bfj7KPAHUADyp1YCJsoLEcL8';
+
+    // Pexels APIのリクエスト 最大８０件まで
+    const pexelsResponse = await axios.get(`https://api.pexels.com/v1/search?query=${word2}&per_page=10`, {
+      headers: {
+        Authorization: pexelsAPIKey,
+      },
+    });
+    const pexelsPhotos = pexelsResponse.data.photos.map((photo) => ({
+      id: photo.id,
+      source: 'pexels',
+      url: photo.src.medium,
+      link: photo.url,
+      photographer: photo.photographer,
+      created_at: photo.created_at,
+    }));
+
+    // Unsplash APIのリクエスト 最大３０件まで
+    const unsplashResponse = await axios.get(`https://api.unsplash.com/search/photos?query=${word2}&per_page=10`, {
+      headers: {
+        Authorization: `Client-ID ${unsplashAPIKey}`,
+      },
+    });
+    const unsplashPhotos = unsplashResponse.data.results.map((photo) => ({
+      id: photo.id,
+      source: 'unsplash',
+      url: photo.urls.regular,
+      link: photo.links.html,
+      photographer: photo.user.name,
+      created_at: photo.created_at,
+    }));
+
+    // PexelsとUnsplashの結果を合わせる
+    const mergedPhotos = [...pexelsPhotos, ...unsplashPhotos];
+
+    // 作成日の新しい順にソートする
+    mergedPhotos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+    // 結果をセットする
+    setPhotos(mergedPhotos);
+  };
+
+  const getPhotoData = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    await searchImages();
+    setLoading(false);
+  };
+
     return(
-        <header>
-            <h1>画像検索アプリ</h1>
-            <p>By <a href="https://unsplash.com/">Unsplash</a>・<a href="https://www.pexels.com/ja-jp/">Pexels</a></p>
-        </header>
+        <>
+            <div class="haedtitle">
+                <h1>画像検索アプリ</h1>
+                <p>By <a href="https://unsplash.com/">Unsplash</a>・<a href="https://www.pexels.com/ja-jp/">Pexels</a></p>
+            </div>
+
+            <Frequent setWord={setWord} searchImages={searchImages} loading = {loading} setLoading={setLoading} />
+            <Form setWord={setWord} word={word} getPhotoData={getPhotoData} />
+            検索文字:{word}
+            <Results photos = {photos} loading = {loading} />
+        </>
     );
 }
  
