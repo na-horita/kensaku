3ff71e76dcc352fd817056b6d1d604f5537db690
hori19 <fourbasic19@gmail.com>
2023-03-31 07:24:55 +0900

    【jsx】Susファイルを空にする。

--- src/pages/Sus.jsx
+++ src/pages/Sus.jsx
@@ -1,32 +1,8 @@
-import ImageShadow from 'react-image-shadow';
-import 'react-image-shadow/assets/index.css';
-
 const Sus = () => {
   return (
-    <>
+    <div>
       <p>練習用</p>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-            </div>
-            <div class="col">
-            <ImageShadow src="https://images.pexels.com/photos/4652275/pexels-photo-4652275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-            <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-            </div>
-            <div class="col">
-              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-            </div>
-            <div class="col">
-              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-            </div>
-          </div> 
-      </div>
-    </>
+    </div>
   );
 };
 
