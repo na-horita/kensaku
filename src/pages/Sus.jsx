2e59ba07d58aac503efd229f02d9708b78fdebd8
hori19 <fourbasic19@gmail.com>
2023-03-30 16:21:42 +0900

    【Bootstrap】row適用

--- src/pages/Sus.jsx
+++ src/pages/Sus.jsx
@@ -3,15 +3,30 @@ import 'react-image-shadow/assets/index.css';
 
 const Sus = () => {
   return (
-    <div>
+    <>
       <p>練習用</p>
-      <div>
-        <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-        <ImageShadow src="https://images.pexels.com/photos/4652275/pexels-photo-4652275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
-        <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
+            </div>
+            <div class="col">
+            <ImageShadow src="https://images.pexels.com/photos/4652275/pexels-photo-4652275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
+            </div>
+            <div class="col">
+              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
+            </div>
+            <div class="col">
+              <ImageShadow src="https://cdn.pixabay.com/photo/2019/12/17/14/07/amaryllis-4701720_960_720.jpg" />
+            </div>
+          </div> 
       </div>
-    </div>
+    </>
   );
 };
 
