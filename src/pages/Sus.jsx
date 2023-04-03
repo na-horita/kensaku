50fb5bb287fa671e508318c87aa51ef48300a021
hori <fourbasic19@gmail.com>
2023-04-04 06:17:58 +0900

    【recoil】他ページでもrecoilの値が使用できることを再確認

--- src/pages/Sus.jsx
+++ src/pages/Sus.jsx
@@ -1,7 +1,12 @@
+import { useRecoilValue } from 'recoil';
+import cartsLength from '../recoil/selectors/cartsLength';
+
 const Sus = () => {
+  const length = useRecoilValue(cartsLength);
+
   return (
     <div>
-      <p>練習用</p>
+      <p>商品数【{ length }個】</p>
     </div>
   );
 };
