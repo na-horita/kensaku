a7e672f8ec15a76722b1d73ebf6a905e39383255
hori <fourbasic19@gmail.com>
2023-04-05 19:26:49 +0900

    【lib】react-grid-galleryにて装飾

--- src/pages/Sus.jsx
+++ src/pages/Sus.jsx
@@ -1,12 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { useRecoilValue } from "recoil";
 import cartsLength from "../recoil/selectors/cartsLength";
+
+import { Gallery } from "react-grid-gallery";
 
 const Sus = () => {
   const length = useRecoilValue(cartsLength);
 
+  const [corpses, setCorpses] = useState([]);
+
+useEffect(() => {
+  async function fetchData() {
+    try {
+      const response = await axios.get('https://kensaku-express.vercel.app/api/size');
+      const updatedCorpses = response.data.corpses.map((corpse) => {
+        return { ...corpse, src: corpse.imgUrl };
+      });
+      setCorpses(updatedCorpses);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchData();
+}, []);
+
   return (
     <div>
-      <p>商品数【{length}個】</p>
+      <h3>楽天お気に入り【{length}個】</h3>
+      <div>
+        <Gallery images={corpses} />
+      </div>
     </div>
   );
 };
