8e1657f4f3aa0878ea5f5c071dd27b21a3ab5e8a
hori <fourbasic19@gmail.com>
2023-09-05 15:16:37 +0900

    【ts】ファイルをtsx化

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,28 +2,28 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import RiseLoader from "react-spinners/RiseLoader";
 import { Button } from "react-bootstrap";
 
-const Results = (props) => {
+const Results = (props: any) => {
   // お気に入りor解除のボタンtoggle
-  const isFavorite = (singleData) => {
+  const isFavorite = (singleData: any) => {
     return (
       props.hopes &&
-      props.hopes.findIndex((hope) => hope.id === singleData.id) !== -1
+      props.hopes.findIndex((hope: any) => hope.id === singleData.id) !== -1
     );
   };
 
   // 押下でindexedDBに登録or解除
-  const handleAddToFavorites = (event, image) => {
+  const handleAddToFavorites = (event: any, image: any) => {
     event.preventDefault();
 
     // 「お気に入り」の配列の中と一致するかのフラグ -1は不一致を表す。
     let existingIndex = -1;
 
     // 「お気に入り」一覧を格納するための配列
-    let newHopes;
+    let newHopes: any;
 
     // 「お気に入り」一覧に既に値があるのかの条件分岐 あれば「お気に入り」とこのイメージが一致するかを計算
     if (props.hopes && props.hopes.length) {
-      existingIndex = props.hopes.findIndex((hope) => hope.id === image.id);
+      existingIndex = props.hopes.findIndex((hope: any) => hope.id === image.id);
       newHopes = [...props.hopes];
     } else {
       // 「お気に入り」一覧が空の配列ならば、newHopesに空の配列を設定
@@ -43,18 +43,11 @@ const Results = (props) => {
       {props.loading && <RiseLoader color={"#26d146"} size={25} margin={40} />}
 
       {!props.loading && (
-        <ResponsiveMasonry
-          columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 5, 1080: 6 }}
-        >
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 5, 1080: 6 }}>
           <Masonry columnsCount={3} gutter="10px">
-            {props.photos.map((singleData, index) => (
+            {props.photos.map((singleData: any, index: any) => (
               <div key={index}>
-                <a
-                  href={singleData.link}
-                  target="_blank"
-                  key={index}
-                  rel="noreferrer"
-                >
+                <a href={singleData.link} target="_blank" key={index} rel="noreferrer">
                   <img src={singleData.url} alt={singleData.photographer} />
                 </a>
                 <p>
