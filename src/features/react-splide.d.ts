b6a4c03aca8bf9676705a7655d4d15fb785c2535
hori <fourbasic19@gmail.com>
2023-09-06 15:12:07 +0900

    【declear】splideに対する型宣言ファイルを作成

---
+++ src/features/react-splide.d.ts
@@ -0,0 +1,9 @@
+// @splidejs/react-splide.d.ts
+declare module "@splidejs/react-splide" {
+  import { FunctionComponent } from "react";
+
+  const Splide: FunctionComponent<any>;
+  const SplideSlide: FunctionComponent<any>;
+
+  export { Splide, SplideSlide };
+}
