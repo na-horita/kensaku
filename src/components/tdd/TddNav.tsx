8e8f09bbd338158a92dd1e2c7afc39a783354069
hori <fourbasic19@gmail.com>
2023-09-25 19:48:52 +0900

    【react-router-dom】現在ページにcssをあてる

--- src/components/tdd/TddNav.tsx
+++ src/components/tdd/TddNav.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const TddNav = () => {
+    const styles = `
+    #root .active {
+      background-color: #ccc;
+    }
+  `;
   return (
     <div className="my-8">
+      <style>{styles}</style>
       <h2 className="mb-8">テスト開発</h2>
-      <Link
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-2 no-underline"
-        to="/tdd/pexels"
-      >
-        ピクセルデータ
-      </Link>
-      <Link
-        className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mx-2 no-underline"
-        to="/tdd/unsplash"
-      >
-        アンスプラッシュデータ
-      </Link>
+      <div className="[&>a]:text-white [&>a]:font-bold [&>a]:py-2 [&>a]:px-4 [&>a]:rounded-full [&>a]:mx-2 [&>a]:no-underline [&>a]:bg-[var(--color)]">
+        <NavLink className="[--color:#3B82F6] hover:bg-blue-300" to="/tdd/pexels">
+          ピクセルデータ
+        </NavLink>
+        <NavLink className="[--color:#EC4899] hover:bg-pink-300" to="/tdd/unsplash">
+          アンスプラッシュデータ
+        </NavLink>
+      </div>
     </div>
   );
-}
+};
 
-export default TddNav
+export default TddNav;
