8d06967eed0c7b02aa3e1d1391a4269654a029e0
hori <fourbasic19@gmail.com>
2023-10-30 20:32:17 +0900

    【vitest】describeでくくる

--- src/__test__/features/sortByNewestCreationData.test.ts
+++ src/__test__/features/sortByNewestCreationData.test.ts
@@ -1,49 +1,51 @@
 import { sortByNewestCreationDate } from "../../features/gathering";
 
-test("基本の日付並び替え", () => {
-  const photos = [
-    { created_at: "2023-10-30T12:00:00Z" },
-    { created_at: "2023-10-29T12:00:00Z" },
-    { created_at: "2023-10-28T12:00:00Z" },
-  ];
+describe("並び替えのユニットテスト", () => {
+  it("基本の日付並び替え", () => {
+    const photos = [
+      { created_at: "2023-10-30T12:00:00Z" },
+      { created_at: "2023-10-29T12:00:00Z" },
+      { created_at: "2023-10-28T12:00:00Z" },
+    ];
 
-  const sortedPhotos = sortByNewestCreationDate(photos);
+    const sortedPhotos = sortByNewestCreationDate(photos);
 
-  expect(sortedPhotos).toEqual([
-    { created_at: "2023-10-30T12:00:00Z" },
-    { created_at: "2023-10-29T12:00:00Z" },
-    { created_at: "2023-10-28T12:00:00Z" },
-  ]);
-});
+    expect(sortedPhotos).toEqual([
+      { created_at: "2023-10-30T12:00:00Z" },
+      { created_at: "2023-10-29T12:00:00Z" },
+      { created_at: "2023-10-28T12:00:00Z" },
+    ]);
+  });
 
-test("基本の日付並び替えの順番待ち替え", () => {
-  const photos = [
-    { created_at: "2023-10-30T12:00:00Z" },
-    { created_at: "2023-10-29T12:00:00Z" },
-    { created_at: "2023-10-28T12:00:00Z" },
-  ];
+  it("基本の日付並び替えの順番待ち替え", () => {
+    const photos = [
+      { created_at: "2023-10-30T12:00:00Z" },
+      { created_at: "2023-10-29T12:00:00Z" },
+      { created_at: "2023-10-28T12:00:00Z" },
+    ];
 
-  const sortedPhotos = sortByNewestCreationDate(photos);
+    const sortedPhotos = sortByNewestCreationDate(photos);
 
-  expect(sortedPhotos).not.toEqual([
-    { created_at: "2023-10-29T12:00:00Z" },
-    { created_at: "2023-10-30T12:00:00Z" },
-    { created_at: "2023-10-28T12:00:00Z" },
-  ]);
-});
+    expect(sortedPhotos).not.toEqual([
+      { created_at: "2023-10-29T12:00:00Z" },
+      { created_at: "2023-10-30T12:00:00Z" },
+      { created_at: "2023-10-28T12:00:00Z" },
+    ]);
+  });
 
-test("created_atの他にも要素があるとき", () => {
-  const photos = [
-    { name: "アリス", created_at: "2023-10-30T12:00:00Z" },
-    { name: "鳥山", created_at: "2023-10-29T12:00:00Z" },
-    { name: "マイケルジャク", created_at: "2023-10-28T12:00:00Z" },
-  ];
+  it("created_atの他にも要素があるとき", () => {
+    const photos = [
+      { name: "アリス", created_at: "2023-10-30T12:00:00Z" },
+      { name: "鳥山", created_at: "2023-10-29T12:00:00Z" },
+      { name: "マイケルジャク", created_at: "2023-10-28T12:00:00Z" },
+    ];
 
-  const sortedPhotos = sortByNewestCreationDate(photos);
+    const sortedPhotos = sortByNewestCreationDate(photos);
 
-  expect(sortedPhotos).toEqual([
-    { name: "アリス", created_at: "2023-10-30T12:00:00Z" },
-    { name: "鳥山", created_at: "2023-10-29T12:00:00Z" },
-    { name: "マイケルジャク", created_at: "2023-10-28T12:00:00Z" },
-  ]);
+    expect(sortedPhotos).toEqual([
+      { name: "アリス", created_at: "2023-10-30T12:00:00Z" },
+      { name: "鳥山", created_at: "2023-10-29T12:00:00Z" },
+      { name: "マイケルジャク", created_at: "2023-10-28T12:00:00Z" },
+    ]);
+  });
 });
--- src/__test__/vitest.test.ts
+++ src/__test__/vitest.test.ts
@@ -14,36 +14,45 @@ const add = (a: number, b: number): number => {
   }
 };
 
-test("合計の上限は、100である", () => {
-  expect(add(10, 90)).toBe(100);
+describe("vitestの基本の書き方", () => {
+  it("合計の上限は、100である", () => {
+    expect(add(10, 90)).toBe(100);
+  });
+
+  it("合計が130でも、100を返す", () => {
+    expect(add(50, 80)).toBe(100);
+  });
+
+  it("引数がマイナス値ならばエラー文を出す", () => {
+    expect(() => add(-50, 80)).toThrow();
+  });
+
+  it("引数がマイナス値ならばエラーでない", () => {
+    expect(() => add(40, 30)).not.toThrow();
+  });
+
+  it("add関数の引数が0から100までの数値でない場合、エラーがスローされることを確認する", () => {
+    expect(() => add(-1, 50)).toThrow(HikisuError);
+    expect(() => add(10, -50)).toThrow(HikisuError);
+    expect(() => add(50, 101)).toThrow(HikisuError);
+    expect(() => add(500, 10)).toThrow(HikisuError);
+    expect(() => add(500, 10)).toThrow(Error);
+    expect(() => add(500, 10)).toThrow();
+  });
+
+  it("返り値が１０か１０以上", () => {
+    expect(add(5, 5)).toBeGreaterThanOrEqual(10);
+    expect(add(5, 5)).toBeGreaterThanOrEqual(8);
+  });
+
+  it("小数点計算", () => {
+    expect(0.1 + 0.4).toBeCloseTo(0.5);
+  });
 });
 
-test("合計が130でも、100を返す", () => {
-  expect(add(50, 80)).toBe(100);
-});
+import { called } from "./moduleA";
 
-test("引数がマイナス値ならばエラー文を出す", () => {
-  expect(() => add(-50, 80)).toThrow();
-});
-
-test("引数がマイナス値ならばエラーでない", () => {
-  expect(() => add(40, 30)).not.toThrow();
-});
-
-test("add関数の引数が0から100までの数値でない場合、エラーがスローされることを確認する", () => {
-  expect(() => add(-1, 50)).toThrow(HikisuError);
-  expect(() => add(10, -50)).toThrow(HikisuError);
-  expect(() => add(50, 101)).toThrow(HikisuError);
-  expect(() => add(500, 10)).toThrow(HikisuError);
-  expect(() => add(500, 10)).toThrow(Error);
-  expect(() => add(500, 10)).toThrow();
-});
-
-test("返り値が１０か１０以上", () => {
-  expect(add(5, 5)).toBeGreaterThanOrEqual(10);
-  expect(add(5, 5)).toBeGreaterThanOrEqual(8);
-});
-
-test("小数点計算", () => {
-    expect(0.1+0.4).toBeCloseTo(0.5);
+test("module確認", () => {
+    const calling = called(8);
+    expect(calling).toBeLessThanOrEqual(7);
 })
