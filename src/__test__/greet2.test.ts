efc0ad139e74d2beb5005518b06a824e68290b9b
hori <fourbasic19@gmail.com>
2023-10-31 16:42:56 +0900

    【vitest】モジュールをスタブに置き換える

--- src/__test__/greet2.test.ts
+++ src/__test__/greet2.test.ts
@@ -1,8 +1,12 @@
 import { greet } from "./greet";
 
-vi.mock("./greet");
+vi.mock("./greet", () => ({
+    greet(name: string) {
+        return `おはようございます!${name}`
+    }
+}));
 
 test("挨拶を返す(本来の実装でない)", () => {
-    expect(greet("太郎")).toBe(undefined);
+    expect(greet("太郎")).toBe("おはようございます!太郎");
 })
 
