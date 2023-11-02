969426f42d5e5b0d18a35b915bf2c5b1a30854d1
hori <fourbasic19@gmail.com>
2023-11-02 13:26:41 +0900

    【zod】オブジェクトスキーマをwordとnumに変更して、parseが無事に通過したらfetchするように

--- src/pages/tdd/unsplash.tsx
+++ src/pages/tdd/unsplash.tsx
@@ -10,9 +10,40 @@ const GetPexels = () => {
   const [pexelsData, setPexelsData] = useState<any>([]);
   const [pexelsDataCustom, setPexelsDataCustom] = useState<ApiUnsplashPhoto[]>([]);
 
-  const inputData: UnsplashApiSchema = {
-    word: "globe",
-    num: 20, //例え30を超えてもapiが勝手に30をセットしてくれる。
+  const [inputData, setInputData] = useState({ word: "cat", num: 8 });
+  const [errors, setErrors] = useState<any>(null);
+  const [isValidaded, setIsValidaded] = useState<boolean>(false);
+
+  const FormData = z.object({
+    word: z
+      .string()
+      .min(1, "wordは1文字以上である必要があります")
+      .max(10, "wordは10文字以下である必要があります")
+      .regex(/^[a-zA-Z0-9]*$/, "英語と数値のみが許可されています"),
+    num: z
+      .number()
+      .int()
+      .min(1, "numの数値は1以上である必要があります")
+      .max(80, "numの数値は80以下である必要があります"),
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    try {
+      FormData.parse(inputData);
+      setErrors(null);
+      setIsValidaded(true);
+      setTimeout(() => {
+        setIsValidaded(false);
+      }, 100);
+      // console.log(inputData);
+    } catch (e: any) {
+      if (e instanceof ZodError) {
+        setErrors(e.flatten().fieldErrors);
+      } else {
+        console.log(e);
+      }
+    }
   };
 
   useEffect(() => {
@@ -28,7 +59,7 @@ const GetPexels = () => {
     } catch (validationError) {
       console.error("入力データが無効です。エラー:", validationError);
     }
-  }, []);
+  }, [isValidaded === true]);
 
   useLayoutEffect(() => {
     const pexelsPhotos: ApiUnsplashPhoto[] = pexelsData.map((photo: ApiUnsplashPhoto) =>
@@ -37,35 +68,18 @@ const GetPexels = () => {
     setPexelsDataCustom(pexelsPhotos);
   }, [pexelsData]);
 
-  const [data, setData] = useState({ email: "", password: "" });
-  const [errors, setErrors] = useState<any>(null);
 
-const FormData = z.object({
-  email: z.string().email({ message: "メールアドレスの形式ではありません。" }),
-  password: z
-    .string()
-    .min(8, { message: "8文字以上入力してください。" })
-    .max(32, { message: "32文字以下で入力してください。" }),
-});
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    try {
-      FormData.parse(data);
-      setErrors(null);
-      console.log(data);
-    } catch (e: any) {
-      if (e instanceof ZodError) {
-        setErrors(e.flatten().fieldErrors);
-      } else {
-        console.log(e);
-      }
-    }
+  // 入力値が変更されたときに呼び出されるハンドラ
+  const handleWordInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInputData({ ...inputData, [name]: value });
   };
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setData({ ...data, [name]: value });
+  // 数値入力が変更されたときに呼び出されるハンドラ
+  const handleNumInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    const value = parseInt(event.target.value, 10); //第二引数は10進数
+    setInputData({ ...inputData, [name]: value });
   };
 
   return (
@@ -78,31 +92,28 @@ const FormData = z.object({
         <form onSubmit={handleSubmit}>
           <div className="flex justify-center gap-x-10 mb-4">
             <div>
-              <label htmlFor="email">Email</label>
+              <label htmlFor="word">Word</label>
               <input
-                id="email"
-                name="email"
+                type="text"
+                name="word"
                 className="border-2 border-slate-400 rounded-md px-2 text-2xl"
-                value={data.email}
-                onChange={handleChange}
+                value={inputData.word}
+                onChange={handleWordInputChange}
               />
             </div>
             <div>
-              <label htmlFor="password">パスワード</label>
+              <label htmlFor="num">Num</label>
               <input
-                id="password"
-                name="password"
+                type="number"
+                name="num"
                 className="border-2 border-slate-400 rounded-md px-2 text-2xl"
-                value={data.password}
-                onChange={handleChange}
-                type="password"
+                value={inputData.num}
+                onChange={handleNumInputChange}
               />
             </div>
           </div>
-          {errors?.email && <p className="text-red-700 font-bold">{errors.email}</p>}
-          {errors?.password && (
-            <p className="text-red-700 font-bold">{errors.password}</p>
-          )}
+          {errors?.word && <p className="text-red-700 font-bold">{errors.word}</p>}
+          {errors?.num && <p className="text-red-700 font-bold">{errors.num}</p>}
           <div className="mb-4">
             <button
               type="submit"
