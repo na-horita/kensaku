205831dc83a15e5781b0e10527dda00adbfd7ce8
hori <fourbasic19@gmail.com>
2023-11-02 12:49:45 +0900

    【zod】オブジェクトスキーマでzodを設定（email,password）

--- src/pages/tdd/unsplash.tsx
+++ src/pages/tdd/unsplash.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect,useLayoutEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
+import { z, ZodError } from "zod";
 import { mapDataToCustomFormat } from "../../features/gathering";
 import { getApiUnsplashData } from "../../api/photo/getApiUnsplashData";
 import { UnsplashApiSchema } from "../../ts/photo";
@@ -11,7 +12,7 @@ const GetPexels = () => {
 
   const inputData: UnsplashApiSchema = {
     word: "globe",
-    num: 20,//例え30を超えてもapiが勝手に30をセットしてくれる。
+    num: 20, //例え30を超えてもapiが勝手に30をセットしてくれる。
   };
 
   useEffect(() => {
@@ -36,12 +37,81 @@ const GetPexels = () => {
     setPexelsDataCustom(pexelsPhotos);
   }, [pexelsData]);
 
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<any>(null);
+
+const FormData = z.object({
+  email: z.string().email({ message: "メールアドレスの形式ではありません。" }),
+  password: z
+    .string()
+    .min(8, { message: "8文字以上入力してください。" })
+    .max(32, { message: "32文字以下で入力してください。" }),
+});
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    try {
+      FormData.parse(data);
+      setErrors(null);
+      console.log(data);
+    } catch (e: any) {
+      if (e instanceof ZodError) {
+        setErrors(e.flatten().fieldErrors);
+      } else {
+        console.log(e);
+      }
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
   return (
     <>
       <TddNav />
       <div>
         <h2>アンスプラッシュデータの実験</h2>
         <p className="text-center text-lg">(一度の検索は30件まで)</p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="flex justify-center gap-x-10 mb-4">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                name="email"
+                className="border-2 border-slate-400 rounded-md px-2 text-2xl"
+                value={data.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">パスワード</label>
+              <input
+                id="password"
+                name="password"
+                className="border-2 border-slate-400 rounded-md px-2 text-2xl"
+                value={data.password}
+                onChange={handleChange}
+                type="password"
+              />
+            </div>
+          </div>
+          {errors?.email && <p className="text-red-700 font-bold">{errors.email}</p>}
+          {errors?.password && (
+            <p className="text-red-700 font-bold">{errors.password}</p>
+          )}
+          <div className="mb-4">
+            <button
+              type="submit"
+              className=" bg-green-500 text-white rounded-lg px-4 py-2"
+            >
+              検索
+            </button>
+          </div>
+        </form>
 
         <table className="border">
           <thead className="border bg-red-400">
