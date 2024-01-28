8ca28cb08e5a5d99d0c59b550cfd99b7f56742b4
hori <fourbasic19@gmail.com>
2024-01-29 02:36:37 +0900

    【fetch】cookieをsetできるようにfetchにcredentials includeを付与する

--- src/api/frequent/getFrequents.ts
+++ src/api/frequent/getFrequents.ts
@@ -2,7 +2,9 @@ import { Frequent } from "../../ts/frequent";
 
 export const getFrequents = async () => {
   try {
-    const response = await fetch("https://kensaku-express.vercel.app/api/frequent");
+    const response = await fetch("https://kensaku-express.vercel.app/api/frequent", {
+      credentials: "include",
+    });
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
