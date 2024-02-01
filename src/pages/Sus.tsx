6a75acf6c17213094d4a245e0e0dff01b49e510f
hori <fourbasic19@gmail.com>
2024-02-01 16:30:30 +0900

    【suspense】カスタムhook→子コンポーネント→親コンポーネントとして記述

--- src/pages/Sus.tsx
+++ src/pages/Sus.tsx
@@ -1,20 +1,35 @@
 import React, { Suspense, useState, useEffect } from "react";
 import SyncLoader from "react-spinners/SyncLoader";
 
-const fetchProfileData = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await response.json();
-  return data;
+type User = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 };
 
-function ProfilePage() {
-  const [data, setData] = useState<any>(null);
+//カスタムhooks jsonデータを取得
+const useProfileData = () => {
+  const [users, setUsers] = useState<User[] | null>(null);
 
   useEffect(() => {
-    fetchProfileData().then(setData);
+    const fetchData = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const jsondata = await response.json();
+      setUsers(jsondata);
+    };
+
+    fetchData();
   }, []);
 
-  if (data === null) {
+  return users;
+};
+
+//子コンポーネント 非同期処理なのでfechが完了するまでと完了後の動作を記述
+function ProfilePage() {
+  const users: User[] | null = useProfileData();
+
+  if (users === null) {
     return (
       <div>
         <SyncLoader color="#36d7b7" />
@@ -24,22 +39,23 @@ function ProfilePage() {
 
   return (
     <div>
-      {data.map((post: any) => (
-        <div key={post.id}>
-          <h4>{post.title}</h4>
-          <p>{post.body}</p>
+      {users.map((user: User) => (
+        <div key={user.id}>
+          <h3>{user.title}</h3>
+          <p>{user.body}</p>
         </div>
       ))}
     </div>
   );
 }
 
-const Sus = () => {
+//親コンポーネント
+function Sus() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback="">
       <ProfilePage />
     </Suspense>
   );
-};
+}
 
 export default Sus;
