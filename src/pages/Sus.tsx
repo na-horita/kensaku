5ea06e0cf47bdfc58ea71e1ae9086f96349ccab2
hori <fourbasic19@gmail.com>
2024-02-01 16:09:32 +0900

    【react】suspense基本

--- src/pages/Sus.tsx
+++ src/pages/Sus.tsx
@@ -1,33 +1,44 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { Suspense, useState, useEffect } from "react";
+import SyncLoader from "react-spinners/SyncLoader";
 
-import { Gallery } from "react-grid-gallery";
+const fetchProfileData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  return data;
+};
 
-const Sus = () => {
-  const [corpses, setCorpses] = useState([]);
+function ProfilePage() {
+  const [data, setData] = useState<any>(null);
 
-useEffect(() => {
-  async function fetchData() {
-    try {
-      const response = await axios.get('https://kensaku-express.vercel.app/api/size');
-      const updatedCorpses = response.data.corpses.map((corpse:any) => {
-        return { ...corpse, src: corpse.imgUrl };
-      });
-      setCorpses(updatedCorpses);
-    } catch (error) {
-      console.error(error);
-    }
+  useEffect(() => {
+    fetchProfileData().then(setData);
+  }, []);
+
+  if (data === null) {
+    return (
+      <div>
+        <SyncLoader color="#36d7b7" />
+      </div>
+    );
   }
-  fetchData();
-}, []);
 
   return (
     <div>
-      <h3>楽天お気に入り【{length}個】</h3>
-      <div>
-        <Gallery images={corpses} />
-      </div>
+      {data.map((post: any) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
+  );
+}
+
+const Sus = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProfilePage />
+    </Suspense>
   );
 };
 
